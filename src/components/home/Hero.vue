906639aee6b5a4278a0d95574bdc828a29760a86
Gblack-Hub <oladiranwale@gmail.com>
2021-06-27 14:21:26 +0100

    Project created and started

---
+++ src/components/home/Hero.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="hero">
+      <h1>Cars</h1>
+    <!-- <v-img
+      :src="require('../assets/logo.svg')"
+      class="my-3"
+      contain
+      height="200"
+    /> -->
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.hero {
+  height: 90vh;
+  background: url('../../assets/hero.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+h1 {
+    font-size: 10rem;
+    color: #fff;
+    padding-top: 20%;
+    padding-left: 2rem;
+    /* margin: ; */
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <Hero />
+  </section>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue';
+import Hero from '@/components/home/Hero.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld,
+    Hero,
+  },
+};
+</script>
